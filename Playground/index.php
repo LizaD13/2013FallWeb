51dec3246362f5efcd308595a6b878a48d726cf2
unknown <n02359129@.active.newpaltz.edu>
2013-09-12 15:20:57 -0400

    nav bar, jumbotron and columns

--- Playground/index.php
+++ Playground/index.php
@@ -18,16 +18,43 @@
     </style>
   </head>
   <body>
-  	<? include 'nav.php' ?>
-    		</div>
-    		<div class="span12">
-    			<h1>Hello World!</h1>
-    			<p>Welcome class of 2013 to Web Server Programming</p>
-    		</div>
-    	
-    </div>
-    </div>
-    </div>
+	  	<? include 'nav.php' ?>
+	    		</div>
+	    		<div class="jumbotron">
+	  <div class="container">
+	    <h1>Hello, world!</h1>
+	    <p>Welcome class of 2013 to Web Server Programming</p>
+	    <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+	  </div>
+	</div>
+	
+	<div class="row">
+		
+		<div class="col-sm-6 col-md-4">
+			  <h2>Heading</h2>
+	          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+	          <p><a class="btn btn-primary" href="#">View details »</a></p>
+			</div>
+			
+			<div class="col-sm-6 col-md-4">
+	          <h2>Important Points</h2>
+	          <ul>
+	          	<li>The three main links in the navbar work</li>
+	          	<li>They are all centralized in one file</li>
+	          	<li>They change apearance to show you which page you are on.</li>
+	          	<li>These colunms start as three columns then reduce as the browser shrinks</li>
+			  </ul>
+	          <p><a class="btn btn-primary" href="#">View details »</a></p>
+			</div>
+			
+			<div class="col-sm-6 col-md-4">
+	          <h2>Heading</h2>
+	          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+	          <p><a class="btn btn-primary" href="#">View details »</a></p>
+  			</div>
+	</div>
+
+ 
 
     <script src="//code.jquery.com/jquery.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
