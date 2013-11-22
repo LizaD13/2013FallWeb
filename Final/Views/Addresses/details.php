085121a1d0daaa638d9d0933a78ebc76320b7172
LizaD13 <Liza@client199-36.wireless.newpaltz.edu>
2013-11-22 12:20:08 -0500

    Start of Updating CRUD

---
+++ Final/Views/Addresses/details.php
@@ -0,0 +1,18 @@
+<div class="container">
+	<dl class="dl-horizontal">
+	  <dt>First Name</dt>
+	  <dd><?=$model['street']?></dd>
+	  <dt>Last Name</dt>
+	  <dd><?=$model['city']?></dd>
+	  <dt>created_at</dt>
+	  <dd><?=$model['created_at']?></dd>
+	  <dt>Password</dt>
+	  <dd><?=$model['state']?></dd>
+	  <br /><dt>UserType</dt>
+	  <dd><?=$model['Users_id']?></dd>
+	  <br /><dt>Address Type</dt>
+	  <dd><?=$model['Address_Type']?></dd>
+	  
+	</dl>
+
+</div>
---
+++ Final/Views/Addresses/edit.php
@@ -0,0 +1,87 @@
+<div class="container">
+	<? $errors = isset($errors) ? $errors : array(); ?>
+	<? if(isset($errors) && count($errors)): ?>
+		<ul>
+		<? foreach ($errors as $key => $value): ?>
+			<li><label><?=$key?>:</label> <?=$value?></li>
+		<? endforeach; ?>
+		</ul>
+	<? endif; ?>
+	<form action="?action=save" method="post"  class="form-horizontal row">
+		<input type="hidden" name="id" value="<?=$model['id']?>" />
+		
+		<div class="form-group <?=isset($errors['street']) ? 'has-error' : ''?>">
+			<label for="street" class="col-sm-2 control-label">First Name</label>
+			<div class="col-sm-10">
+				<input type="text" name="street" id="street" placeholder="First Name" class="form-control " value="<?=$model['street']?>"  />
+			</div>
+			<span><?=@$errors['street']?></span>
+		</div>
+		
+		<div class="form-group <?=isset($errors['city']) ? 'has-error' : ''?>">
+			<label for="city" class="col-sm-2 control-label">Last Name</label>
+			<div class="col-sm-10">
+				<input type="text" name="city" id="city" placeholder="Last Name" class="form-control " value="<?=$model['city']?>"  />
+			</div>
+			<span><?=@$errors['city']?></span>
+		</div>
+		
+		<div class="form-group <?=isset($errors['state']) ? 'has-error' : ''?>">
+			<label for="state" class="col-sm-2 control-label">state</label>
+			<div class="col-sm-10">
+				<input type="state" name="state" id="state" placeholder="state" class="form-control "  value="<?=$model['state']?>" />
+			</div>
+			<span><?=@$errors['state']?></span>
+		</div>
+		
+		<div class="form-group <?=isset($errors['zip']) ? 'has-error' : ''?>">
+			<label for="zip" class="col-sm-2 control-label">zip</label>
+			<div class="col-sm-10">
+				<input type="zip" name="zip" id="zip" placeholder="zip" class="form-control "  value="<?=$model['zip']?>" />
+			</div>
+			<span><?=@$errors['zip']?></span>
+		</div>
+		
+		<div class="form-group <?=isset($errors['Address_Type']) ? 'has-error' : ''?>">
+			<label for="Address_Type" class="col-sm-2 control-label">Address Type</label>
+			<div class="col-sm-10">
+				<select name="Address_Type" id="Address_Type" class="form-control ">
+					<? foreach (Keywords::GetSelectListFor(3) as $keywordRs): ?>
+						<option value="<?=$keywordRs['id']?>"><?=$keywordRs['Name']?></option>
+					<? endforeach; ?>
+				</select>	
+			</div>
+			<span><?=@$errors['Address_Type']?></span>
+		</div>
+		
+		<div class="form-group <?=isset($errors['Users_id']) ? 'has-error' : ''?>">
+			<label for="Users_id" class="col-sm-2 control-label">User Type</label>
+			<div class="col-sm-10">
+				<select name="Users_id" id="Users_id" class="form-control ">
+					<? foreach (Users::Get() as $keywordRs): ?>
+						<option value="<?=$keywordRs['id']?>"><?=$keywordRs['LastName']?></option>
+					<? endforeach; ?>
+				</select>	
+			</div>
+			<span><?=@$errors['Users_id']?></span>
+		</div>
+
+		
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<input type="submit" class="form-control btn btn-primary" value="Save" />
+			</div>
+		</div>
+	</form>
+</div>
+<script type="text/javascript">
+	$(function(){
+		$("#Address_Type").val(<?=$model['Address_Type']?>);
+		$("#Users_id").val(<?=$model['Users_id']?>);
+	})	
+</script>
+
+
+
+
+
