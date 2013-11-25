43f895d6546285b58c7102ebddf087442070e37a
LizaD13 <Liza@liza-davidsons-macbook.local>
2013-11-25 13:24:59 -0500

    Front Page and more Address CRUD

--- Final/Models/Addresses.php
+++ Final/Models/Addresses.php
@@ -5,20 +5,99 @@
  */
 class Addresses {
 	
-	static public function Get()
+	static public function Get($id=null)
 	{
-		$ret = array();
-		$conn = GetConnection();
-		$result = $conn->query('SELECT * FROM 2013Fall_Users');
-		
-		while ($rs = $result->fetch_assoc()) {
-			$ret[] = $rs;
+		if(isset($id)){
+			$sql = "	SELECT U.*,US.LastName as 'User' K.Name as 'Address Type'
+						FROM 
+						Addresses U
+							Join 
+							Keywords K ON U.`Address_Type`=K.id
+							Join
+							Users US ON U.'Users_id' =US.id
+						WHERE U.id=$id
+					";
+			return fetch_one($sql);			
+		}else{
+			$sql = "	SELECT U.*,US.LastName as 'User' K.Name as 'Address Type'
+						FROM 
+						Addresses U
+							Join 
+							Keywords K ON U.`Address_Type`=K.id
+							Join
+							Users US ON U.'Users_id' =US.id
+						
+					";
+			return fetch_all($sql);			
 		}
-		
-		$conn->close();
-		return $ret;
 	}
 	
+	static public function Blank()
+	{
+		return array( 'id'=>null, 'street'=> null,'city'=> null,'state'=> null,'zip'=> null, 'Users_id'=> null,'Address_Type'=> null );
+	}
+	
+	static public function Save($row)
+	{
+		$conn = GetConnection();
+		$row2 = Addresses::Encode($row, $conn);
+		if($row['id']){
+			$sql =	" UPDATE Addresses "
+				.	" Set LastName='$row2[LastName]', street='$row2[street]', city='$row2[city]', state='$row2[state]', zip='$row2[zip]', Users_id='$row2[Users_id]' Address_Type='$row2[Address_Type]' "
+				.	" WHERE id=$row2[id] ";
+		}else{
+			$sql = 	" Insert Into Addresses (LastName, street, city, state, zip, Users_id, Address_Type) "
+				.	" Values ($row2[LastName]', street='$row2[street]', city='$row2[city]', state='$row2[state]', zip='$row2[zip]', Users_id='$row2[Users_id]' Address_Type='$row2[Address_Type]') ";			
+		}
+		
+		$conn->query($sql);
+		//echo $sql; 
+		$error = $conn->error;		
+		$conn->close();
+		//$error = "dd";
+		if($error){
+			return array('db_error' => $error);
+		}else {
+			return false;
+		}
+	}
+	
+	static public function Delete($id)
+	{
+		$conn = GetConnection();
+		$sql =	" DELETE From Addresses WHERE id=$id ";
+				
+		$conn->query($sql);
+		//echo $sql; 
+		$error = $conn->error;		
+		$conn->close();
+		//$error = "dd";
+		if($error){
+			return array('db_error' => $error);
+		}else {
+			return false;
+		}		
+	}
+	
+	static public function Validate($row)
+	{
+		$errors = array();
+		if(!$row['street']) $errors['street'] = 'street required';
+		if(!$row['city']) $errors['city'] = 'city required';
+		if(!$row['state']) $errors['state'] = 'state required';
+		if(!$row['zip']) $errors['zip'] = 'zip required';
+		if(!is_numeric($row['Address_Type'])) $errors['Address_Type'] = 'must be a number';
+		if(!$row['Address_Type']) $errors['Address_Type'] = 'id required';
+		
+		return count($errors) ? $errors : null;
+	}
+	
+	static function Encode($row, $conn)
+	{
+		$row2 = array();
+		foreach ($row as $key => $value) {
+			$row2[$key] = $conn->real_escape_string($value);		
+		}
+		return $row2;
+	}
 }
-	
-	
--- Final/Views/Addresses/index.php
+++ Final/Views/Addresses/index.php
@@ -1,7 +1,85 @@
-<?php 
+<?php
 include_once '../../inc/_global.php';
 
-$model = Addresses::Get();
-$view  = 'list.php';
+@$action = $_REQUEST['action'];
+@$format = $_REQUEST['format'];
+$errors = null;
 
-include '../Shared/_Layout.php';
+switch ($action) {
+	case 'details':
+		$model  = Users::Get($_REQUEST['id']);
+		$view 	= 'details.php';
+		$title	= "Details for: $model[FirstName] $model[LastName]"	;	
+		break;
+		
+	case 'new':
+		$model = Users::Blank();
+		$view 	= 'edit.php';		
+		$title	= "Create New User"	;	
+		break;
+	
+	case 'save':
+		$errors = Users::Validate($_REQUEST);
+		if(!$errors){
+			$errors = Users::Save($_REQUEST);			
+		}
+		if(!$errors){
+			if($format == 'plain' || $format == 'json'){
+				$view = 'item.php';
+				$rs = $model = Users::Get($_REQUEST['id']);
+			}else{
+				header("Location: ?status=Saved&id=$_REQUEST[id]");
+				die();
+			}
+		}else{
+			$model = $_REQUEST;
+			$view = 'edit.php';
+			$title	= "Edit: $model[FirstName] $model[LastName]"	;	
+		}			
+			
+		break;
+		
+	case 'edit':
+		$model  = Users::Get($_REQUEST['id']);
+		$view 	= 'edit.php';		
+		$title	= "Edit: $model[FirstName] $model[LastName]"	;	
+		break;
+		
+	case 'delete':
+		if(isset($_POST['id'])){
+			$errors = Users::Delete($_REQUEST['id']);			
+			if(!$errors){
+				header("Location: ?");
+				die();
+			}							
+		}
+		$model  = Users::Get($_REQUEST['id']);
+		$view 	= 'delete.php';					
+		$title	= "Edit: $model[FirstName] $model[LastName]"	;	
+		break;
+	
+	default:
+		$model  = Users::Get();
+		$view 	= 'list.php';
+		$title	= 'Users';		
+		break;
+}
+
+switch ($format) {
+	case 'dialog':
+		include '../Shared/_DialogLayout.php';				
+		break;
+	
+	case 'plain':
+		include $view;
+		break;
+		
+	case 'json';
+		echo json_encode(array('model' => $model, 'erroes' => $errors));
+		break;
+	
+	default:
+		include '../Shared/_Layout.php';		
+		break;
+}
+
